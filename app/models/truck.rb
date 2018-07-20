d51c2f71a32fd796934dac5a8a792c5cad887cc2
Lee gun woo <casio3612@gmail.com>
2018-07-20 11:18:09 +0000

    truck validates 추가

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -4,4 +4,5 @@ class Truck < ApplicationRecord
     has_many :postings
     has_many :truck_reviews
     validates :user, uniqueness: true
+    validates :truck_name, :truck_category, presence: true
 end
