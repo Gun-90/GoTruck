5e4e6bdca2d718778891ba785f6738372696cccc
Lee gun woo <casio3612@gmail.com>
2018-07-20 08:02:43 +0000

    add nickname validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :nickname, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :truck
   has_one :profile
   has_many :followings
   has_many :likes
-  
+  validates :user, uniqueness: true
   
 end
