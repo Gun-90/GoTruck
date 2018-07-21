51c92575b33b0f8717713385ef2b9e1384b7a4d3
Lee gun woo <casio3612@gmail.com>
2018-07-21 13:02:18 +0000

    set root page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :posting_reviews
   resources :likes
   resources :followings
-  
+  root "trucks#index"
  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
