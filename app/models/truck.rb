4c5d4120aead316d67e87e7449a9b650754421cf
Lee gun woo <casio3612@gmail.com>
2018-07-21 15:54:21 +0000

    트럭 삭제시 종속된 메누 포스팅 트럭리뷰 삭제

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,8 +1,8 @@
 class Truck < ApplicationRecord
     belongs_to :user
-    has_many :menus
-    has_many :postings
-    has_many :truck_reviews
+    has_many :menus, dependent: :delete_all
+    has_many :postings, dependent: :delete_all
+    has_many :truck_reviews, dependent: :delete_all
     validates :user, uniqueness: true
     validates :truck_name, :truck_category, presence: true
 end
