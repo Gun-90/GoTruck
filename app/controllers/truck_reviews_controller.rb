de467ef47a0b43e4b8102c1483f95d52bc66329a
Lee gun woo <casio3612@gmail.com>
2018-07-23 01:53:13 +0000

    트럭 리뷰 등록 뷰와 컨트롤러 완료

--- app/controllers/truck_reviews_controller.rb
+++ app/controllers/truck_reviews_controller.rb
@@ -1,10 +1,13 @@
 class TruckReviewsController < ApplicationController
     before_action :authenticate_user!
     def create
-        # @truck_review = Truck_review.new(params.require(:review).permit())
+        @truck_review = TruckReview.new(params.require(:truck_review).permit(:truck_review_content,:truck_review_image))
+        @truck_review.user_id = current_user.id
+        @truck_review.truck_id = params[:truck_id]
+        @truck_review.save
         
-        if @truck_reviews.save
-        end
+        # if @truck_reviews.save
+        # end
     end
     
     def destroy
