79ab3d72a7c0b3d7ece39002104da89c0afba72a
Lee gun woo <casio3612@gmail.com>
2018-07-23 15:11:48 +0000

    트럭 리뷰 작성시 트럭페이지 새로고침

--- app/controllers/truck_reviews_controller.rb
+++ app/controllers/truck_reviews_controller.rb
@@ -1,10 +1,20 @@
 class TruckReviewsController < ApplicationController
     before_action :authenticate_user!
     def create
+        @truck = Truck.find(params[:truck_id])
         @truck_review = TruckReview.new(params.require(:truck_review).permit(:truck_review_content,:truck_review_image))
         @truck_review.user_id = current_user.id
         @truck_review.truck_id = params[:truck_id]
-        @truck_review.save
+        
+        respond_to do |format|
+          if @truck_review.save
+            format.html { redirect_to @truck, notice: 'Truck Review was successfully created.' }
+            format.json { render :show, status: :created, location: @truck }
+          else
+            format.html { render :new }
+            format.json { render json: @truck.errors, status: :unprocessable_entity }
+         end
+    end
         
         # if @truck_reviews.save
         # end
@@ -15,7 +25,7 @@ class TruckReviewsController < ApplicationController
         @truck_review = TruckReview.find(params[:id])
         @truck_review.destroy
         respond_to do |format|
-          format.html { redirect_to truck_path(@truck), notice: 'Truck Review was successfully destroyed.' }
+          format.html { redirect_to truck_path(@truck.id), notice: 'Truck Review was successfully destroyed.' }
           format.json { head :no_content }
         end
     end
