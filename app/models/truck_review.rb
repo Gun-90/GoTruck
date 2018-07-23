0316f1e8aa748c76e6327da20a2b6b3abacb8dd6
Lee gun woo <casio3612@gmail.com>
2018-07-23 15:16:26 +0000

    트럭 리뷰 발리데이트 추가

--- app/models/truck_review.rb
+++ app/models/truck_review.rb
@@ -2,4 +2,5 @@ class TruckReview < ApplicationRecord
   belongs_to :user
   belongs_to :truck
   mount_uploader :truck_review_image, TruckReviewPictureUploader
+  validates :truck_review_content ,presence: true
 end
